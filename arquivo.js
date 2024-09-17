03baa7271c4a50529c002ffd921833ac9462787b
Gks-code <eduardo.barbosa.santos24@escola.pr.gov.br>
2024-09-17 08:18:51 -0300

    adicionado

---
+++ arquivo.js
@@ -0,0 +1,14 @@
+let fruta = ['maça', 'uva', 'banana'];
+console.log(fruta);
+
+fruta.push('manga')
+console.log(fruta);
+
+fruta.unshift('melancia')
+console.log(fruta);
+
+fruta.shift()
+console.log(fruta);
+
+fruta.pop()
+console.log(fruta);
